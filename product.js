bbc338201a0bda657476dae4abed54b33fade62c
wewelski <f.roowell@gmail.com>
2023-06-10 23:28:10 +0800

    utilized mongoose's built-in validation

--- product.js
+++ product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://127.0.0.1:27017/storeApp', { useNewUrlParser: true, useUnifiedTopology: true})
 .then (() => {
   console.log('Connection OPEN!');
 })
